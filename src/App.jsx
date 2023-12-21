997ecd6fefee38cd794d640a720e2bd12d16e1cf
panatyuki <admiral.dancho@gmail.com>
2023-12-21 12:25:39 +0700

    feat: add a function add a new text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  const updateGreeting = () => {
+    const newGreeting = document.getElementById("greeting-message").value;
+    setGreeting(newGreeting);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          // ลองใช้ placeholder แบบที่ครูน็อตมีสอนเพิ่มครับ รู้สึกสวยงามดี
+          placeholder="Enter Message Here"
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateGreeting}>Update text</button>
       </div>
     </div>
   );
